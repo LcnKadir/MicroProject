1f26bee477b3e837bc3b6d70b6c0c0dd005fa042
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-07-02 15:51:36 +0300

    API and MVC projects created.

---
+++ MiniMicroProject.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniMicroProject.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
